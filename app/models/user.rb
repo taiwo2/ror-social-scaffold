2174a61cc87019a310b94c0e02b092599e794862
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-16 19:19:35 +0500

    add friendship associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  
+  has_many :active_friendships, class_name: 'Friendship', foreign_key: :requester_id, dependent: :destroy
+  has_many :requesters, through: :active_friendships, source: :receiver
+
+  has_many :passive_friendships, class_name: 'Friendship', foreign_key: :receiver_id, dependent: :destroy
+  has_many :receivers, through: :passive_friendships, source: :requester
+
 end
