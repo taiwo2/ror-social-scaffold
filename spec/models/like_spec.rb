8c765c55b5bb001a32a0bed6aa5e84e8fae9225b
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-17 14:34:26 +0500

    fix indentation in likes spec

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,16 +4,12 @@ RSpec.describe Like, type: :model do
   subject(:like) { build(:like) }
 
   describe 'associations' do
-    it { is_expected.to have_many(:likes) }
-
-    it { is_expected.to have_many(:comments) }
+    it { is_expected.to belong_to(:post) }
 
     it { is_expected.to belong_to(:user) }
   end
 
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:content) }
-
-    it { is_expected.to validate_length_of(:content).is_at_most(1000) }
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:post_id) }
   end
 end
