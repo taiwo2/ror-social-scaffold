b5d8e30b085e71256bb45d8b355ed454ab9995f8
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-17 14:50:02 +0500

    make changes in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,15 +22,20 @@ RSpec.describe User, type: :model do
       it 'send friend request to other user with pending status' do
         user = create(:user)
         another_user = create(:user)
+
         user.send_friend_request_to(another_user)
-        expect(user.requests.map(&:id)).to match_array([another_user.id])
+
+        expect(user.requesters.map(&:id)).to match_array([another_user.id])
         expect(user.active_friendships.first.status).to eq('pending') 
       end 
     end
+
     it 'raise error if user send multiple requests' do
       user = create(:user)
       another_user = create(:user)
+
       user.send_friend_request_to(another_user)
+
       expect { user.send_friend_request_to(another_user) }.to raise_error(ActiveRecord::RecordNotUnique)
     end
   end
@@ -39,10 +44,12 @@ RSpec.describe User, type: :model do
     it 'accepts other users friend requests' do
       user = create(:user)
       another_user = create(:user)
+
       user.send_friend_request_to(another_user)
       another_user.accept_friend_request(user)
-      expect(another_user.passive_friendships.first.status).to eq('accept')
-      expect(user.active_friendships.first.status).to eq('accept')
+
+      expect(another_user.passive_friendships.first.status).to eq('accepted')
+      expect(user.active_friendships.first.status).to eq('accepted')
     end
   end
 
@@ -51,8 +58,10 @@ RSpec.describe User, type: :model do
     it 'delete other users friend requests' do
       user = create(:user)
       another_user = create(:user)
+
       user.send_friend_request_to(another_user)
       another_user.delete_friend_request(user)
+
       expect(another_user.receivers.map(&:id)).to match_array([])
       expect(user.requesters.map(&:id)).to match_array([])
     end
@@ -62,16 +71,12 @@ RSpec.describe User, type: :model do
     it 'list all friends of user' do
       user = create(:user)
       another_user = create(:user)
+
       user.send_friend_request_to(another_user)
       another_user.accept_friend_request(user)
+
       expect(user.friends.map(&:id)).to match_array([another_user.id])
       expect(another_user.friends.map(&:id)).to match_array([user.id])
-    end
-    it 'does not list users that have pending status' do
-      user = create(:user)
-      another_user = create(:user)
-      user.send_friend_request_to(another_user)
-      expect(user.friends.count).to eq be_zero
     end
   end
 
@@ -79,18 +84,11 @@ RSpec.describe User, type: :model do
     it 'returns true if the user is friend of another user' do
       user = create(:user)
       another_user = create(:user)
+
       user.send_friend_request_to(another_user)
       another_user.accept_friend_request(user)
+
       expect(user.friend?(another_user)).to be_truthy
     end
-    it 'returns false if the user is not a friend of another user' do
-      user = create(:user)
-      another_user = create(:user)
-      user.send_friend_request_to(another_user)
-      expect(user.friend?(another_user)).to be_falsy
-    end
   end
-
-  
-
 end
