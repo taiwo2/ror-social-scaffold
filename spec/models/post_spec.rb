2a2fe82cd71e71aadb84f6127236e9769e8c5045
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-17 14:34:41 +0500

    fix indentation in post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,6 +14,6 @@ RSpec.describe Post, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:content) }
 
-    it { is_expected.to validate_length_of(:content).is_at_most(1000) }
+    it { is_expected.to validate_length_of(:content).is_at_most(1000).with_message('1000 characters in post is the maximum allowed.') } # rubocop:disable Layout/LineLength
   end
 end
