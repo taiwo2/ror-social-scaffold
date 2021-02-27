573f63c4d16c5102eec46d431488818f483e5175
taiwo2 <adetonat@ymail.com>
2021-02-27 08:07:44 +0100

    update show and _action,html.erb

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -13,9 +13,8 @@ module UserHelper
   end
 
   def cancle(user)
-    unless current_user.friend_request_pending_to?(user)
-      link_to('cancle', user_friendships_path(user), method: :delete)
-    end
+    return link_to('cancle', user_friendships_path(user), method: :delete) if current_user.friend_request_pending_to?(user)
+    
   end
 
   def inivite(user)
